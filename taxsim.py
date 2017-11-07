4b8de75c315a4ffbe59f6fcf638c5bda78dab743
Amir El-Sibaie <amir.elsibaie@gmail.com>
2017-11-07 09:16:14 -0500

    add qualified income calculator and get_brackets() function
    also adds rounding for outputs and calculates avg_effective_tax_rate

--- taxsim.py
+++ taxsim.py
@@ -38,8 +38,13 @@ def calc_federal_taxes(taxpayer):
     income_tax_before_credits = tax_funcs.fed_ordinary_income_tax(policy, taxpayer, taxable_income)
     print("income_tax_before_credits", income_tax_before_credits)
 
-    # TODO: Add qualified income/capital gains taxes
-    
+    # Qualified income/capital gains
+    # TODO: Check for bugs
+    qualified_income_tax = tax_funcs.fed_qualified_income(policy, taxpayer, taxable_income, income_tax_before_credits)
+    print("qualified_income_tax", qualified_income_tax)
+    income_tax_before_credits = min(income_tax_before_credits, qualified_income_tax)
+    print("Tax on taxable income:", income_tax_before_credits)
+
     # AMT
     amt = tax_funcs.fed_amt(policy, taxpayer, deduction_type, deductions, agi, pease_limitation_amt)
     print("amt", amt)
@@ -62,17 +67,22 @@ def calc_federal_taxes(taxpayer):
         federal_income_tax = max(amt, income_tax_after_credits)
     else:
         federal_income_tax = income_tax_after_credits
+
+    # Federal income tax
+    federal_income_tax = round(federal_income_tax, 2)
     print("federal_income_tax", federal_income_tax)
 
     # Tax burden
-    tax_burden = federal_income_tax + employee_payroll_tax
+    tax_burden = round(federal_income_tax + employee_payroll_tax, 2)
     print("tax_burden", tax_burden)
 
-    # Tax wedge
-    tax_wedge = federal_income_tax + employee_payroll_tax + employer_payroll_tax
-    print("tax_wedge", tax_wedge)
+    # Average effective tax rate
+    avg_effective_tax_rate = round((tax_burden / gross_income) * 100, 2)
+    print("avg_effective_tax_rate", avg_effective_tax_rate)
 
-    print("")
+    # Tax wedge
+    tax_wedge = round(federal_income_tax + employee_payroll_tax + employer_payroll_tax, 2)
+    print("tax_wedge", tax_wedge, "\n")
 
 
 for taxpayer in taxpayers:
