429621748707bcdde7491854adf64b3e20ee6b64
Amir El-Sibaie <amir.elsibaie@gmail.com>
2017-11-07 15:44:13 -0500

    replace print statments with summary `pprint` statistics

--- taxsim.py
+++ taxsim.py
@@ -1,6 +1,7 @@
 import csv_parser
 import tax_funcs
 from collections import OrderedDict
+from pprint import pprint
 
 TAXPAYERS_FILE = "taxpayers.csv"
 
@@ -19,33 +20,23 @@ def calc_federal_taxes(taxpayer, policy):
     results = OrderedDict()
     # Gross income
     gross_income = taxpayer['ordinary_income1'] + taxpayer['ordinary_income2'] + taxpayer['business_income'] + taxpayer['ss_income'] + taxpayer['qualified_income']
-    print("gross_income", gross_income)
     results["gross_income"] = gross_income
 
     # Payroll taxes
     employee_payroll_tax, employer_payroll_tax = tax_funcs.fed_payroll(policy, taxpayer)
-    print("employee_payroll_tax", employee_payroll_tax)
-    print("employer_payroll_tax", employer_payroll_tax)
     results["employee_payroll_tax"] = employee_payroll_tax
     results["employer_payroll_tax"] = employer_payroll_tax
 
     # Income after tax-deferred retirement contributions
     ordinary_income_after_401k = taxpayer['ordinary_income1'] + taxpayer['ordinary_income2'] - taxpayer['401k_contributions']
-    print("ordinary_income_after_401k", ordinary_income_after_401k)
     results["ordinary_income_after_401k"] = ordinary_income_after_401k
 
     # AGI
     agi = tax_funcs.fed_agi(policy, taxpayer, ordinary_income_after_401k)
-    print("agi", agi)
     results["agi"] = agi
 
     # Taxable income
     taxable_income, deduction_type, deductions, personal_exemption_amt, pease_limitation_amt = tax_funcs.fed_taxable_income(policy, taxpayer, agi)
-    print("taxable_income", taxable_income)
-    print("deduction_type", deduction_type)
-    print("deductions", deductions)
-    print("personal_exemption_amt", personal_exemption_amt)
-    print("pease_limitation_amt", pease_limitation_amt)
     results["taxable_income"] = taxable_income
     results["deduction_type"] = deduction_type
     results["deductions"] = deductions
@@ -54,36 +45,29 @@ def calc_federal_taxes(taxpayer, policy):
 
     # Ordinary income tax
     income_tax_before_credits = tax_funcs.fed_ordinary_income_tax(policy, taxpayer, taxable_income)
-    print("income_tax_before_credits", income_tax_before_credits)
     results["income_tax_before_credits"] = income_tax_before_credits
 
     # Qualified income/capital gains
     # TODO: Check for bugs
     qualified_income_tax = tax_funcs.fed_qualified_income(policy, taxpayer, taxable_income, income_tax_before_credits)
-    print("qualified_income_tax", qualified_income_tax)
     income_tax_before_credits = min(income_tax_before_credits, qualified_income_tax)
-    print("income_tax_before_credits", income_tax_before_credits)
     results["qualified_income_tax"] = qualified_income_tax
     results["income_tax_before_credits"] = income_tax_before_credits
 
     # AMT
     amt = tax_funcs.fed_amt(policy, taxpayer, deduction_type, deductions, agi, pease_limitation_amt)
-    print("amt", amt)
     results["amt"] = amt
 
     # CTC
     ctc = tax_funcs.fed_ctc(policy, taxpayer, agi)
-    print("ctc", ctc)
     results["ctc"] = ctc
 
     # EITC
     eitc = tax_funcs.fed_eitc(policy, taxpayer)
-    print("eitc", eitc)
     results["eitc"] = eitc
 
     # Tax after credits
     income_tax_after_credits = income_tax_before_credits - ctc - eitc
-    print("income_tax_after_credits", income_tax_after_credits)
     results["income_tax_after_credits"] = income_tax_after_credits
 
     # AMT check
@@ -95,22 +79,18 @@ def calc_federal_taxes(taxpayer, policy):
 
     # Federal income tax
     federal_income_tax = round(federal_income_tax, 2)
-    print("federal_income_tax", federal_income_tax)
     results["federal_income_tax"] = federal_income_tax
 
     # Tax burden
     tax_burden = round(federal_income_tax + employee_payroll_tax, 2)
-    print("tax_burden", tax_burden)
     results["tax_burden"] = tax_burden
 
     # Average effective tax rate
     avg_effective_tax_rate = round((tax_burden / gross_income) * 100, 2)
-    print("avg_effective_tax_rate", avg_effective_tax_rate)
     results["avg_effective_tax_rate"] = avg_effective_tax_rate
 
     # Tax wedge
     tax_wedge = round(federal_income_tax + employee_payroll_tax + employer_payroll_tax, 2)
-    print("tax_wedge", tax_wedge)
     results["tax_wedge"] = tax_wedge
 
     return results
@@ -124,33 +104,23 @@ def calc_house_2018_taxes(taxpayer, policy):
     results = OrderedDict()
     # Gross income
     gross_income = taxpayer['ordinary_income1'] + taxpayer['ordinary_income2'] + taxpayer['business_income'] + taxpayer['ss_income'] + taxpayer['qualified_income']
-    print("gross_income", gross_income)
     results["gross_income"] = gross_income
 
     # Payroll taxes
     employee_payroll_tax, employer_payroll_tax = tax_funcs.fed_payroll(policy, taxpayer)
-    print("employee_payroll_tax", employee_payroll_tax)
-    print("employer_payroll_tax", employer_payroll_tax)
     results["employee_payroll_tax"] = employee_payroll_tax
     results["employer_payroll_tax"] = employer_payroll_tax
 
     # Income after tax-deferred retirement contributions
     ordinary_income_after_401k = taxpayer['ordinary_income1'] + taxpayer['ordinary_income2'] - taxpayer['401k_contributions']
-    print("ordinary_income_after_401k", ordinary_income_after_401k)
     results["ordinary_income_after_401k"] = ordinary_income_after_401k
 
     # AGI
     agi = tax_funcs.fed_agi(policy, taxpayer, ordinary_income_after_401k)
-    print("agi", agi)
     results["agi"] = agi
 
     # Taxable income
     taxable_income, deduction_type, deductions, personal_exemption_amt, pease_limitation_amt = tax_funcs.fed_taxable_income(policy, taxpayer, agi)
-    print("taxable_income", taxable_income)
-    print("deduction_type", deduction_type)
-    print("deductions", deductions)
-    print("personal_exemption_amt", personal_exemption_amt)
-    print("pease_limitation_amt", pease_limitation_amt)
     results["taxable_income"] = taxable_income
     results["deduction_type"] = deduction_type
     results["deductions"] = deductions
@@ -159,7 +129,6 @@ def calc_house_2018_taxes(taxpayer, policy):
 
     # Ordinary income tax
     income_tax_before_credits = tax_funcs.fed_ordinary_income_tax(policy, taxpayer, taxable_income - taxpayer["business_income"]) + (taxpayer["business_income"] * 0.25)
-    print("debug: income_tax_before_credits and 12% po", income_tax_before_credits)
 
     # NEW: Phaseout of benefit of the 12-percent bracket
     po_amount = 0
@@ -169,32 +138,26 @@ def calc_house_2018_taxes(taxpayer, policy):
         benefit = policy["income_tax_rates"][-1] * brackets[2] - policy["income_tax_rates"][0] * brackets[2]
         po_amount = min(benefit, 0.06 * (agi - lower_rate_po_threshold[taxpayer["filing_status"]]))  # Hardcoded policy
     income_tax_before_credits = income_tax_before_credits + po_amount
-    print("income_tax_before_credits", income_tax_before_credits)
     results["income_tax_before_credits"] = income_tax_before_credits
 
     # Qualified income/capital gains
     # TODO: Check for bugs
     # NEW: new house_2018_qualified_income function
     qualified_income_tax = tax_funcs.house_2018_qualified_income(policy, taxpayer, taxable_income, income_tax_before_credits, po_amount)
-    print("qualified_income_tax", qualified_income_tax)
     income_tax_before_credits = min(income_tax_before_credits, qualified_income_tax)
-    print("income_tax_before_credits", income_tax_before_credits)
     results["qualified_income_tax"] = qualified_income_tax
     results["income_tax_before_credits"] = income_tax_before_credits
 
     # AMT
     amt = tax_funcs.fed_amt(policy, taxpayer, deduction_type, deductions, agi, pease_limitation_amt)
-    print("amt", amt)
     results["amt"] = amt
 
     # CTC
     ctc = tax_funcs.fed_ctc(policy, taxpayer, agi)
-    print("ctc", ctc)
     results["ctc"] = ctc
 
     # EITC
     eitc = tax_funcs.fed_eitc(policy, taxpayer)
-    print("eitc", eitc)
     results["eitc"] = eitc
 
     # NEW: Personal credit
@@ -208,7 +171,6 @@ def calc_house_2018_taxes(taxpayer, policy):
 
     # Tax after credits
     income_tax_after_credits = income_tax_before_credits - ctc - eitc  # TODO: check refundable portion of CTC
-    print("income_tax_after_credits", income_tax_after_credits)
     results["income_tax_after_credits"] = income_tax_after_credits
 
     # AMT check
@@ -220,22 +182,18 @@ def calc_house_2018_taxes(taxpayer, policy):
 
     # Federal income tax
     federal_income_tax = round(federal_income_tax, 2)
-    print("federal_income_tax", federal_income_tax)
     results["federal_income_tax"] = federal_income_tax
 
     # Tax burden
     tax_burden = round(federal_income_tax + employee_payroll_tax, 2)
-    print("tax_burden", tax_burden)
     results["tax_burden"] = tax_burden
 
     # Average effective tax rate
     avg_effective_tax_rate = round((tax_burden / gross_income) * 100, 2)
-    print("avg_effective_tax_rate", avg_effective_tax_rate)
     results["avg_effective_tax_rate"] = avg_effective_tax_rate
 
     # Tax wedge
     tax_wedge = round(federal_income_tax + employee_payroll_tax + employer_payroll_tax, 2)
-    print("tax_wedge", tax_wedge)
     results["tax_wedge"] = tax_wedge
 
     return results
@@ -243,11 +201,16 @@ def calc_house_2018_taxes(taxpayer, policy):
 
 current_law_results = []
 house_2018_results = []
-for filer in taxpayers:
-    print("\n" + "Starting current law calculator")
-    current_law_results.append(calc_federal_taxes(filer, current_law_policy))
-    print("\n" + "Starting house 2018 calculator")
-    house_2018_results.append(calc_house_2018_taxes(filer, house_2018_policy))
+for i in range(len(taxpayers)):
+    filer = taxpayers[i]
+    print("\n" + "Current law - filer #" + str(i + 1))
+    current_law_result = calc_federal_taxes(filer, current_law_policy)
+    current_law_results.append(current_law_result)
+    pprint(current_law_result)
+    print("\n" + "House 2018 - filer #" + str(i + 1))
+    house_2018_result = calc_house_2018_taxes(filer, house_2018_policy)
+    house_2018_results.append(house_2018_result)
+    pprint(house_2018_result)
 
 csv_parser.write_results(current_law_results, CURRENT_LAW_RESULTS)
 csv_parser.write_results(house_2018_results, HOUSE_2018_RESULTS)
