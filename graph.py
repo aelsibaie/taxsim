658a4f6c5bc481f662fff8e3c49c89f8a41e6054
Tom VanAntwerp <tom@tomvanantwerp.com>
2017-11-09 11:03:50 -0500

    More spacing in graph code for human eyes

--- graph.py
+++ graph.py
@@ -26,9 +26,17 @@ for i in range(1, 10000):
                                     ('other_itemized', 0)])
     income = i * 100
     default_taxpayer['ordinary_income1'] = income
-    current_law_result = taxsim.calc_federal_taxes(default_taxpayer, taxsim.current_law_policy)
+
+    current_law_result = taxsim.calc_federal_taxes(
+        default_taxpayer,
+        taxsim.current_law_policy
+    )
     current_law_result_list.append(current_law_result)
-    house_2018_result = taxsim.calc_house_2018_taxes(default_taxpayer, taxsim.house_2018_policy)
+
+    house_2018_result = taxsim.calc_house_2018_taxes(
+        default_taxpayer,
+        taxsim.house_2018_policy
+    )
     house_2018_result_list.append(house_2018_result)
 
 current_law_df = pd.DataFrame(current_law_result_list)
@@ -39,10 +47,22 @@ fig = plt.figure()
 ax = plt.axes()
 ax.yaxis.set_major_formatter(FuncFormatter('{0:.0%}'.format))
 ax.xaxis.set_major_formatter(FuncFormatter('${:,.0f}'.format))
-ax.plot(current_law_df["gross_income"], current_law_df["avg_effective_tax_rate"], drawstyle='steps-pre', label='Current Law')
-ax.plot(house_2018_df["gross_income"], house_2018_df["avg_effective_tax_rate"], drawstyle='steps-pre', label='House 2018 Proposal')
+ax.plot(
+    current_law_df["gross_income"],
+    current_law_df["avg_effective_tax_rate"],
+    drawstyle='steps-pre',
+    label='Current Law'
+)
+ax.plot(
+    house_2018_df["gross_income"],
+    house_2018_df["avg_effective_tax_rate"],
+    drawstyle='steps-pre',
+    label='House 2018 Proposal'
+)
 ax.legend(loc='upper left')
-ax.set_title('Average Effective Federal Income Tax Rate by Gross Income (Single Filers)')
+ax.set_title(
+    'Average Effective Federal Income Tax Rate by Gross Income (Single Filers)'
+    )
 ax.set_xlabel('Gross Income')
 ax.set_ylabel('Average Effective Tax Rate')
 fig.set_size_inches(8, 4)
