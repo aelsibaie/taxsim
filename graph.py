1775c7c4111f4aee2b8ffa2b3698f57a86d741d5
Amir El-Sibaie <amir.elsibaie@gmail.com>
2017-11-11 18:25:18 -0500

    refactor graph.py

--- graph.py
+++ graph.py
@@ -8,33 +8,49 @@ from tqdm import tqdm
 plt.style.use('ggplot')
 
 
-def make_graph(main_income_type, file_name, filing_status, child_dep, income_ratios, payroll, step, top_range=10000):
+def make_graph(main_income_type,
+               file_name,
+               filing_status,
+               child_dep,
+               income_ratios,
+               payroll,
+               step,
+               top_range=10000):
+
     current_law_result_list = []
     house_2018_result_list = []
     senate_2018_result_list = []
 
-    for i in tqdm(range(1, top_range), desc=file_name, unit='taxpayer', leave=False):
+    for i in tqdm(
+            range(1, top_range),
+            desc=file_name,
+            unit='taxpayer',
+            leave=False):
         income = i * step
-        default_taxpayer = OrderedDict([('filing_status', filing_status),
-                                        ('child_dep', child_dep),
-                                        ('nonchild_dep', 0),
-                                        ('ordinary_income1', income * income_ratios["ordinary"]),
-                                        ('ordinary_income2', 0),
-                                        ('business_income', income * income_ratios["business"]),
-                                        ('ss_income', income * income_ratios["ss"]),
-                                        ('qualified_income', income * income_ratios["qualified"]),
-                                        ('401k_contributions', 0),
-                                        ('medical_expenses', 0),
-                                        ('sl_income_tax', 0),
-                                        ('sl_property_tax', 0),
-                                        ('interest_paid', 0),
-                                        ('charity_contributions', 0),
-                                        ('other_itemized', 0)])
-        current_law_result = taxsim.calc_federal_taxes(default_taxpayer, taxsim.current_law_policy)
+        default_taxpayer = OrderedDict(
+            [('filing_status', filing_status),
+             ('child_dep', child_dep),
+             ('nonchild_dep', 0),
+             ('ordinary_income1', income * income_ratios["ordinary"]),
+             ('ordinary_income2', 0),
+             ('business_income', income * income_ratios["business"]),
+             ('ss_income', income * income_ratios["ss"]),
+             ('qualified_income', income * income_ratios["qualified"]),
+             ('401k_contributions', 0),
+             ('medical_expenses', 0),
+             ('sl_income_tax', 0),
+             ('sl_property_tax', 0),
+             ('interest_paid', 0),
+             ('charity_contributions', 0),
+             ('other_itemized', 0)])
+        current_law_result = taxsim.calc_federal_taxes(
+            default_taxpayer, taxsim.current_law_policy)
         current_law_result_list.append(current_law_result)
-        house_2018_result = taxsim.calc_house_2018_taxes(default_taxpayer, taxsim.house_2018_policy)
+        house_2018_result = taxsim.calc_house_2018_taxes(
+            default_taxpayer, taxsim.house_2018_policy)
         house_2018_result_list.append(house_2018_result)
-        senate_2018_result = taxsim.calc_senate_2018_taxes(default_taxpayer, taxsim.senate_2018_policy)
+        senate_2018_result = taxsim.calc_senate_2018_taxes(
+            default_taxpayer, taxsim.senate_2018_policy)
         senate_2018_result_list.append(senate_2018_result)
 
     current_law_df = pd.DataFrame(current_law_result_list)
@@ -42,9 +58,18 @@ def make_graph(main_income_type, file_name, filing_status, child_dep, income_rat
     senate_2018_df = pd.DataFrame(senate_2018_result_list)
 
     # Save CSVs
-    current_law_df.to_csv(taxsim.RESULTS_DIR + file_name + '-current_law_graph_data.csv')
-    house_2018_df.to_csv(taxsim.RESULTS_DIR + file_name + '-house_2018_graph_data.csv')
-    senate_2018_df.to_csv(taxsim.RESULTS_DIR + file_name + '-senate_2018_graph_data.csv')
+    current_law_df.to_csv(
+        taxsim.RESULTS_DIR +
+        file_name +
+        '-current_law_graph_data.csv')
+    house_2018_df.to_csv(
+        taxsim.RESULTS_DIR +
+        file_name +
+        '-house_2018_graph_data.csv')
+    senate_2018_df.to_csv(
+        taxsim.RESULTS_DIR +
+        file_name +
+        '-senate_2018_graph_data.csv')
 
     filing_status_string = "Single"
     if filing_status == 1:
@@ -71,11 +96,29 @@ def make_graph(main_income_type, file_name, filing_status, child_dep, income_rat
     ax = plt.axes()
     ax.yaxis.set_major_formatter(FuncFormatter('{0:.0%}'.format))
     ax.xaxis.set_major_formatter(FuncFormatter('${:,.0f}'.format))
-    ax.plot(current_law_df["gross_income"], current_law_df[rate_type], drawstyle='steps-pre', label='Current Law')
-    ax.plot(house_2018_df["gross_income"], house_2018_df[rate_type], drawstyle='steps-pre', label='House 2018 Proposal')
-    ax.plot(senate_2018_df["gross_income"], senate_2018_df[rate_type], drawstyle='steps-pre', label='Senate 2018 Proposal')
+    ax.plot(
+        current_law_df["gross_income"],
+        current_law_df[rate_type],
+        drawstyle='steps-pre',
+        label='Current Law')
+    ax.plot(
+        house_2018_df["gross_income"],
+        house_2018_df[rate_type],
+        drawstyle='steps-pre',
+        label='House 2018 Proposal')
+    ax.plot(
+        senate_2018_df["gross_income"],
+        senate_2018_df[rate_type],
+        drawstyle='steps-pre',
+        label='Senate 2018 Proposal')
     ax.legend(loc='upper left')
-    ax.set_title('Average Federal Income Tax Rate by Gross Income, ' + filing_status_string + " Filer, " + child_string + ", " + main_income_type)
+    ax.set_title(
+        'Average Federal Income Tax Rate by Gross Income, ' +
+        filing_status_string +
+        " Filer, " +
+        child_string +
+        ", " +
+        main_income_type)
     ax.set_xlabel('Gross Income')
     ax.set_ylabel('Average Tax Rate (' + payroll_string + ')')
     fig.set_size_inches(12, 6)
@@ -133,7 +176,7 @@ graphs = [
             "ss": 0.0,
             "qualified": 0.5},
         "payroll": 0,
-        "step": 100
+        "step": 200
     },
     {
         "main_income_type": "Ordinary Income",
@@ -191,4 +234,10 @@ graphs = [
 
 if __name__ == '__main__':
     for graph in tqdm(graphs, desc='Rendering graphs', unit='graph'):
-        make_graph(graph["main_income_type"], graph["file_name"], graph["filing_status"], graph["child_dep"], graph["income_ratios"], graph["payroll"], graph["step"])
+        make_graph(graph["main_income_type"],
+                   graph["file_name"],
+                   graph["filing_status"],
+                   graph["child_dep"],
+                   graph["income_ratios"],
+                   graph["payroll"],
+                   graph["step"])
