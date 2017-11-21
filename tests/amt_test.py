0660e17f192d8b1ea35e7a4a658467808ead8902
Amir El-Sibaie <amir.elsibaie@gmail.com>
2017-11-21 14:14:30 -0500

    add new tests and codecov

--- tests/amt_test.py
+++ tests/amt_test.py
@@ -3,6 +3,6 @@ from context import *
 
 def test_amt():
     taxpayer = misc_funcs.create_taxpayer()
-    taxpayer['qualified_income'] = 1000000
+    taxpayer['qualified_income'] = taxsim.current_law_policy["amt_exemption_po_threshold"][0]
     result = taxsim.calc_federal_taxes(taxpayer, taxsim.current_law_policy)
     assert result['amt'] > 0
