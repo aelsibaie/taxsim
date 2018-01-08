d4afd637ba363e150165fe7a9cc9319657378551
Amir El-Sibaie <amir.elsibaie@gmail.com>
2018-01-08 12:49:33 -0500

    change print to log and add new try-except block

--- api.py
+++ api.py
@@ -3,7 +3,6 @@ import taxsim.taxsim as taxsim
 import taxsim.misc_funcs as misc_funcs
 from collections import OrderedDict
 from datetime import datetime
-from pprint import pprint
 
 from flask import Flask, abort, request, jsonify
 app = Flask(__name__)
@@ -41,36 +40,40 @@ curl --request POST \
 @app.route("/taxcalc/tcja_submit", methods=['POST'])
 def hello():
     if not request.json:
-        print("[" + datetime.now().isoformat(' ') + "] Received non-json data from " + request.remote_addr)
+        taxsim.logging.warn("Received non-json data from " + request.remote_addr)
         abort(400)
-    print("[" + datetime.now().isoformat(' ') + "] Received input taxpayer from " + request.remote_addr)
-    pprint(request.json)
+    taxsim.logging.info("Received input taxpayer from " + request.remote_addr)
+    taxsim.logging.debug(request.json)
     submission = request.json
     taxpayer = misc_funcs.create_taxpayer()
-    taxpayer['filing_status'] = submission['filing_status']
-    taxpayer['child_dep'] = submission['child_dep']
-    taxpayer['nonchild_dep'] = submission['nonchild_dep']
-    taxpayer['ordinary_income1'] = submission['ordinary_income1']
-    taxpayer['ordinary_income2'] = submission['ordinary_income2']
-    taxpayer['business_income'] = submission['business_income']
-    taxpayer['ss_income'] = submission['ss_income']
-    taxpayer['qualified_income'] = submission['qualified_income']
-    taxpayer['401k_contributions'] = submission['401k_contributions']
-    taxpayer['medical_expenses'] = submission['medical_expenses']
-    taxpayer['sl_income_tax'] = submission['sl_income_tax']
-    taxpayer['sl_property_tax'] = submission['sl_property_tax']
-    taxpayer['interest_paid'] = submission['interest_paid']
-    taxpayer['charity_contributions'] = submission['charity_contributions']
-    taxpayer['other_itemized'] = submission['other_itemized']
+    try:
+        taxpayer['filing_status'] = submission['filing_status']
+        taxpayer['child_dep'] = submission['child_dep']
+        taxpayer['nonchild_dep'] = submission['nonchild_dep']
+        taxpayer['ordinary_income1'] = submission['ordinary_income1']
+        taxpayer['ordinary_income2'] = submission['ordinary_income2']
+        taxpayer['business_income'] = submission['business_income']
+        taxpayer['ss_income'] = submission['ss_income']
+        taxpayer['qualified_income'] = submission['qualified_income']
+        taxpayer['401k_contributions'] = submission['401k_contributions']
+        taxpayer['medical_expenses'] = submission['medical_expenses']
+        taxpayer['sl_income_tax'] = submission['sl_income_tax']
+        taxpayer['sl_property_tax'] = submission['sl_property_tax']
+        taxpayer['interest_paid'] = submission['interest_paid']
+        taxpayer['charity_contributions'] = submission['charity_contributions']
+        taxpayer['other_itemized'] = submission['other_itemized']
+    except KeyError:
+        taxsim.logging.warn("Received malformed json data from " + request.remote_addr)
+        abort(400)
     try:
         result = tax_calc(taxpayer, policy)
         alt_result = alt_tax_calc(taxpayer, alt_policy)
     except:
-        print("[" + datetime.now().isoformat(' ') + "] Taxpayer failed input validation for " + request.remote_addr)
+        taxsim.logging.warn("Taxpayer failed input validation for " + request.remote_addr)
         abort(400)
-    print("[" + datetime.now().isoformat(' ') + "] Calculations complete for " + request.remote_addr)
+    taxsim.logging.info("Calculations complete for " + request.remote_addr)
     return jsonify({"pre-tcja": result, "tcja": alt_result})
 
 if __name__ == "__main__":
-    app.config['DEBUG'] = True
-    app.run(port=8080, debug=True, host='0.0.0.0')
+    #app.debug = True
+    app.run(port=8080, host='0.0.0.0')
