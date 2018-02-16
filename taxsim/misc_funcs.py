cc4e12cc9d0aa273de53585935a3c894fc144ca6
Amir El-Sibaie <amir.elsibaie@gmail.com>
2018-02-16 14:36:37 -0500

    remove unused git_version() function

--- taxsim/misc_funcs.py
+++ taxsim/misc_funcs.py
@@ -54,33 +54,6 @@ def create_taxpayer():
          ('other_itemized', 0)])
     return default_taxpayer
 
-# Return the git revision as a string
-# https://github.com/numpy/numpy/blob/20c3c2a11d4fbbff1db5717c9fc7c3e35c671ed4/setup.py#L71-L93
-
-
-def git_version():
-    def _minimal_ext_cmd(cmd):
-        # construct minimal environment
-        env = {}
-        for k in ['SYSTEMROOT', 'PATH', 'HOME']:
-            v = os.environ.get(k)
-            if v is not None:
-                env[k] = v
-        # LANGUAGE is used on win32
-        env['LANGUAGE'] = 'C'
-        env['LANG'] = 'C'
-        env['LC_ALL'] = 'C'
-        out = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=env).communicate()[0]
-        return out
-
-    try:
-        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
-        GIT_REVISION = out.strip().decode('ascii')
-    except OSError:
-        GIT_REVISION = "Unknown"
-
-    return GIT_REVISION
-
 
 def validate_taxpayer(taxpayer):
     # Check for proper inputs
--- tests/misc_test.py
+++ tests/misc_test.py
@@ -4,11 +4,6 @@ import pytest
 policy = taxsim.current_law_policy
 
 
-def test_git_version():
-    git_version = misc_funcs.git_version()
-    assert len(git_version) == 40
-
-
 def test_load_taxpayers():
     taxpayers = csv_parser.load_taxpayers('taxpayers.csv')
     assert taxpayers[0]['ordinary_income1'] == 30000
