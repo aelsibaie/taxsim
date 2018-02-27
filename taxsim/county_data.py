d383b730494d5f85983132707458f299caca747d
Amir El-Sibaie <amir.elsibaie@gmail.com>
2018-02-27 17:01:19 -0500

    large overhaul to county level data module

--- taxsim/county_data.py
+++ taxsim/county_data.py
@@ -11,6 +11,7 @@ import pandas
 import logging
 from tqdm import tqdm
 from collections import OrderedDict
+import math
 
 from . import taxsim
 from . import misc_funcs
@@ -27,10 +28,18 @@ def process_county_data():
     logging.info("Loading: " + COUNTY_DATA_AGI)
     data_agi = pandas.read_csv(COUNTY_DATA_AGI)
 
-    logging.info("Loading: " + COUNTY_DATA_NOAGI)
-    data_noagi = pandas.read_csv(COUNTY_DATA_NOAGI)
-
+    #logging.info("Loading: " + COUNTY_DATA_NOAGI)
+    #data_noagi = pandas.read_csv(COUNTY_DATA_NOAGI)
     # TODO: Merge the two datasets before running the calculator
+
+    # drop negative or 0 AGIs
+    data_agi = data_agi[data_agi.AGI_STUB != 1]
+    data_agi = data_agi[data_agi.N00200 != 0]
+
+    # we only want NY
+    data_agi = data_agi[data_agi.STATE == "NY"]
+
+    scale = 1000
 
     rows = []
     for row in tqdm(data_agi.itertuples()):
@@ -54,51 +63,77 @@ def process_county_data():
         row_calcs['percent_single'] = div(row.MARS1, row.N1)
         row_calcs['percent_joint'] = div(row.MARS2, row.N1)
         row_calcs['percent_hoh'] = div(row.MARS4, row.N1)
-        # percent_other may include filers who are married but filing separately
-        # TODO: write test
-        row_calcs['percent_other'] = max(0, 1 - row_calcs['percent_single'] - row_calcs['percent_joint'] - row_calcs['percent_hoh'])
-        row_calcs['percent_elderly'] = div(row.ELDERLY, row.N1)
+        
+        row_calcs['filing_status'] = 0
+        if row_calcs['percent_joint'] > row_calcs['percent_single']:
+            row_calcs['filing_status'] = 1
+        elif (row_calcs['percent_hoh'] > row_calcs['percent_single']) and (row_calcs['percent_hoh'] > row_calcs['percent_joint']):
+            row_calcs['filing_status'] = 2
+
+
+        #row_calcs['percent_other'] = max(0, 1 - row_calcs['percent_single'] - row_calcs['percent_joint'] - row_calcs['percent_hoh'])
+        #row_calcs['percent_elderly'] = div(row.ELDERLY, row.N1)
+        
         row_calcs['percent_itemizing'] = div(row.N04470, row.N1)
+        if row_calcs['percent_itemizing'] > 0.5:
+            row_calcs['itemizing_flag'] = 1
+        else:
+            row_calcs['itemizing_flag'] = 0
 
         # CHILDREN
-        row_calcs['number_children'] = div(row.NUMDEP, (row.MARS2 + row.MARS4))  # only divide by filing status' capable of having children
-        row_calcs['rounded_children'] = round(row_calcs['number_children'], 0)
+        #row_calcs['number_children'] = div(row.NUMDEP, (row.MARS2 + row.MARS4))  # only divide by filing status' capable of having children
+        #row_calcs['rounded_children'] = round(row_calcs['number_children'], 0)
+
+        # household size and new method for children
+        row_calcs['household_size'] = round(div(row.N2, row.N1), 0)
+        if row_calcs['filing_status'] == 1:
+            children = row_calcs['household_size'] - 2
+        else:
+            children = row_calcs['household_size'] - 1
+        row_calcs['children'] = max(0, children)
 
         # INCOMES
-        row_calcs['taxable_ss_inc'] = div(row.A02500, row.N02500)  # only includes the taxable portion of SS income
+        #row_calcs['taxable_ss_inc'] = div(row.A02500, row.N02500) * scale  # only includes the taxable portion of SS income
 
-        row_calcs['wage_inc'] = div(row.A00200, row.N00200)
+        row_calcs['agi'] = div(row.A00100, row.N00200) * scale
 
-        row_calcs['ordinary_div_inc'] = div(row.A00600, row.N00600)
-        row_calcs['qual_div_inc'] = div(row.A00650, row.N00650)
-        row_calcs['cap_gains_inc'] = div(row.A01000, row.N01000)  # unsure if long term cap gains
+        ordinary_income = row.A00200 + row.A00300 + row.A00600
 
-        row_calcs['partnership_scorp_inc'] = div(row.A26270, row.N26270)
-        row_calcs['business_professional_inc'] = div(row.A00900, row.N00900)
+        #row_calcs['wage_inc'] = div(row.A00200, row.N00200) * scale
+        #row_calcs['taxable_int_inc'] = div(row.A00300, row.N00300) * scale
+        #row_calcs['ordinary_div_inc'] = div(row.A00600, row.N00600) * scale
+
+        qualified_income = row.A00650 + row.A01000
+
+        share_ordinary = div(ordinary_income, (ordinary_income + qualified_income))
+        share_qualified = div(qualified_income, (ordinary_income + qualified_income))
+
+        row_calcs['ordinary_income'] = share_ordinary * row_calcs['agi']
+        row_calcs['qualified_income'] = share_qualified * row_calcs['agi']
+
+
+        #row_calcs['qual_div_inc'] = div(row.A00650, row.N00650) * scale
+        #row_calcs['cap_gains_inc'] = div(row.A01000, row.N01000) * scale # assuming long term
+
+        #row_calcs['partnership_scorp_inc'] = div(row.A26270, row.N26270) * scale
+        #row_calcs['business_professional_inc'] = div(row.A00900, row.N00900) * scale
 
         # DEDUCTIONS
-        row_calcs['total_itemized'] = div(row.A04470, row.N04470)
+        row_calcs['total_itemized'] = div(row.A04470, row.N04470) * scale
 
-        row_calcs['state_local_income'] = div(row.A18425, row.N04470)
-        row_calcs['state_local_sales'] = div(row.A18450, row.N04470)
-        # Pick either sales or income tax deduction
-        row_calcs['state_local_sales_or_inc'] = max(
-            row_calcs['state_local_sales'],
-            row_calcs['state_local_income'])
-        row_calcs['state_local_prop'] = div(row.A18500, row.N04470)
+        income_or_sales = max(row.A18425, row.A18450)
+        imputed_itemized_deductions = income_or_sales + row.A18500 + row.A19300 + row.A19700
+        percent_income_or_sales_deduction = div(income_or_sales, imputed_itemized_deductions)
+        percent_property_deduction = div(row.A18500, imputed_itemized_deductions)
+        percent_mort_int_deduction = div(row.A19300, imputed_itemized_deductions)
+        percent_charity_deduction = div(row.A19700, imputed_itemized_deductions)
 
-        calculated_taxes_paid = row_calcs['state_local_prop'] + row_calcs['state_local_sales_or_inc']
-        ratio_inc_sales = div(row_calcs['state_local_sales_or_inc'], calculated_taxes_paid)
-        ratio_prop = div(row_calcs['state_local_prop'], calculated_taxes_paid)
+        row_calcs['imputed_income_or_sales_deduction'] = percent_income_or_sales_deduction * row_calcs['total_itemized']
+        row_calcs['imputed_property_deduction'] = percent_property_deduction * row_calcs['total_itemized']
+        row_calcs['imputed_mort_int_deduction'] = percent_mort_int_deduction * row_calcs['total_itemized']
+        row_calcs['imputed_charity_deduction'] = percent_charity_deduction * row_calcs['total_itemized']
 
-        row_calcs['taxes_paid_total'] = div(row.A18300, row.N04470)  # TODO: write test
 
-        row_calcs['state_local_sales_or_inc'] = ratio_inc_sales * row_calcs['taxes_paid_total']
-        row_calcs['state_local_prop'] = ratio_prop * row_calcs['taxes_paid_total']
-
-        row_calcs['mort_int'] = div(row.A19300, row.N04470)
-
-        row_calcs['charity_cont'] = div(row.A19700, row.N04470)
 
         # Append final results to list
         rows.append(row_calcs)
